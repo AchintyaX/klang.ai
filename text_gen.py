09381a31aef07d503ab43e60d77d441571983f74
Anant Shankhdhar <anantshankhdhar0808@gmail.com>
2021-02-09 00:54:49 +0530

    text gen from file and recording

---
+++ text_gen.py
@@ -0,0 +1,36 @@
+import deepspeech
+import time
+import wave
+import numpy as np 
+import pyaudio
+from audio_gen import user_audio 
+
+
+class text_gen():
+	def __init__(self,alpha=0.75,beta=1.85):
+		self.alpha = alpha
+		self.beta = beta
+
+	def text_from_file (self, filename):
+		lm_file_path = 'deepspeech-0.9.3-models.pbmm'
+		model_file_path = 'deepspeech-0.9.3-models.pbmm'
+		model = deepspeech.Model(model_file_path)
+		scorer_file_path = 'deepspeech-0.9.3-models.scorer'
+		model.enableExternalScorer(scorer_file_path)
+		lm_alpha = self.alpha
+		lm_beta = self.beta
+		model.setScorerAlphaBeta(lm_alpha,lm_beta)
+		beam_width = 500
+		model.setBeamWidth(beam_width)
+		w = wave.open(filename, 'r')
+		frames = w.getnframes()
+		buffer =  w.readframes(frames)
+		data16 = np.frombuffer(buffer,dtype = np.int16)
+		text = model.stt(data16)
+		print(text)
+
+	def text_from_recording(self):
+		recording = user_audio()
+		recording.record()
+		self.text_from_file('samples/test.wav')
+
