de1533372b7f48310a13a224213ec9026b536f61
AchintyaX <achintyashankhdhar@gmail.com>
2021-02-13 02:15:58 +0530

    added streaming support for direct transcription from directory

--- text_gen.py
+++ text_gen.py
@@ -4,14 +4,19 @@ import wave
 import numpy as np 
 import pyaudio
 from audio_gen import user_audio 
-
+import glob
 
 class text_gen():
+	# HyperParameter tuning 
 	def __init__(self,alpha=0.75,beta=1.85):
 		self.alpha = alpha
 		self.beta = beta
 
-	def text_from_file (self, filename):
+	# Generating Text from Speech 
+	# filename - filepath of the file you wanna transcribe 
+	# also caches the last transcription task for quick access.
+	# stram - used to check if we are stream a folder or working on a single file 
+	def text_from_file (self, filename, stream=False):
 		lm_file_path = 'deepspeech-0.9.3-models.pbmm'
 		model_file_path = 'deepspeech-0.9.3-models.pbmm'
 		model = deepspeech.Model(model_file_path)
@@ -26,12 +31,39 @@ class text_gen():
 		frames = w.getnframes()
 		buffer =  w.readframes(frames)
 		data16 = np.frombuffer(buffer,dtype = np.int16)
+
 		text = model.stt(data16)
+		if stream == False :
+			arr = [text]
+			with open('temp.txt', 'w') as file:
+				for line in arr:
+					file.write("".join(line)+' \n')
+			file.close()
 		return text 
 
+	# Generating text from live audio
 	def text_from_recording(self):
 		recording = user_audio()
 		recording.record()
 		text = self.text_from_file('samples/test.wav')
 		return text 
+
+	# Getting Text from all the audio files present in a folder 
+	# path - filepath to the directory containing all the audio files
+	# at the moment we only support .wav extension	
+	def folder_stream(self, path):
+		wav_files = glob.glob(path+'*.wav')
+		arr = []
+		for i in wav_files:
+			x = self.text_from_file(i, stream=True)
+			arr.append(x)
+
+		with open('temp.txt', 'w') as file :
+			for line in arr:
+				file.write("".join(line)+' \n')
+			file.close()
+
+		return arr 
+
+
 
